aea865942573256072321ba3b67575d1f7fc329b
Antonio Villasenor <antonio.villasenor@csic.es>
2023-03-09 11:58:29 +0100

    ign2nor.sh now does not need date command

--- scripts/ign2nor.sh
+++ scripts/ign2nor.sh
@@ -18,15 +18,6 @@ norfile=$prefix.nor
 
 command -v ign2nor > /dev/null 2>&1 || { echo "ERROR: ign2nor executable does not exist"; exit 1; }
 
-if command -v date > /dev/null && date --version > /dev/null 2>&1; then
-    DATE=date
-elif command -v gdate > /dev/null && gdate --version > /dev/null 2>&1; then
-    DATE=gdate
-else
-    echo "ERROR: no GNU date command in this system"
-    exit 1
-fi
-
 /bin/rm -f tmp.nor $norfile
 
 ign2nor << END > /dev/null
@@ -37,7 +28,8 @@ END
 status=$?
 [[ $status -ne 0 || ! -s tmp.nor ]] && { echo "ERROR: conversion of $imsfile to Nordic failed"; exit 1; }
 
-date_string=$( $DATE "+%y-%m-%d %H:%M" )
-sed "s/ ACTION:NEW                OP/ ACTION:NEW $date_string OP/" tmp.nor > $norfile
+# New version of ign2nor adds date to nordic file
+#date_string=$( $DATE "+%y-%m-%d %H:%M" )
+#sed "s/ ACTION:NEW                OP/ ACTION:NEW $date_string OP/" tmp.nor > $norfile
 
-/bin/rm -f tmp.nor
+/bin/mv -f tmp.nor $norfile
